60db654f5f5d6c2a19d1dbd73cd21110a217260d
irfanbabar <irfanbabar99@gmail.com>
2017-10-16 02:29:49 +0500

    Create Login page and added sessions controller

--- PhotoGallery/Controllers/SessionsController.cs
+++ PhotoGallery/Controllers/SessionsController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using PhotoGallery.Models;
+using System.Security.Cryptography;
 
 namespace PhotoGallery.Controllers
 {
@@ -23,12 +24,41 @@ namespace PhotoGallery.Controllers
         {
             _context.Dispose();
         }
-
+        [Route("Login")]
         public ActionResult New()
         {
+            ViewBag.Title = "Sign In";
             var user = new User();
             return View(user);
         }
 
+        [HttpPost]
+        public ActionResult Create(User user_params)
+        {
+            if (user_params == null)
+                return RedirectToAction("New");
+            var user = _context.User.SingleOrDefault(m=>m.Email == user_params.Email);
+            if (user == null)
+                return RedirectToAction("New");
+            //var password_salt = CreateSalt();
+            //var hash = GenerateHash(user_params.Password, password_salt);
+            //return Content(hash);
+            return RedirectToAction("New");
+        }
+
+        //private static string GenerateHash(string value, string salt)
+        //{
+        //    byte[] data = System.Text.Encoding.ASCII.GetBytes(salt + value);
+        //    data = System.Security.Cryptography.MD5.Create().ComputeHash(data);
+        //    return Convert.ToBase64String(data);
+        //}
+
+        //public static string CreateSalt()
+        //{
+        //    RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+        //    byte[] buff = new byte[32];
+        //    rng.GetBytes(buff);
+        //    return Convert.ToBase64String(buff);
+        //}
     }
 }
