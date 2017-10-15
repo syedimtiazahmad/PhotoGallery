b0a257df446cd4ae1ff4510d54fc3daa9687d3f2
irfanbabar <irfanbabar99@gmail.com>
2017-10-15 19:21:31 +0500

    added session view page

---
+++ PhotoGallery/Controllers/SessionsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PhotoGallery.Models;
+
+namespace PhotoGallery.Controllers
+{
+    public class SessionsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public SessionsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        public ActionResult New()
+        {
+            var user = new User();
+            return View(user);
+        }
+
+    }
+}
