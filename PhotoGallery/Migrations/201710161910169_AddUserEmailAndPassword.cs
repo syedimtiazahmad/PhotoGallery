8776022abfedf51d63488084c6295a874fcab179
irfanbabar <irfanbabar99@gmail.com>
2017-10-17 00:39:31 +0500

    implemented create action in registrtion and integrated validation for user

---
+++ PhotoGallery/Migrations/201710161910169_AddUserEmailAndPassword.cs
@@ -0,0 +1,20 @@
+namespace PhotoGallery.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddUserEmailAndPassword : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Users", "Email", c => c.String(nullable: false));
+            AlterColumn("dbo.Users", "Password", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Users", "Password", c => c.String());
+            AlterColumn("dbo.Users", "Email", c => c.String());
+        }
+    }
+}
--- PhotoGallery/Models/User.cs
+++ PhotoGallery/Models/User.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using System.ComponentModel.DataAnnotations;
 namespace PhotoGallery.Models
 {
     public class User
@@ -11,7 +11,12 @@ namespace PhotoGallery.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int age { set; get; }
+
+        [Required]
+        [EmailAddress]
         public string Email { set; get; }
+
+        [Required]
         public string Password { set; get; }
     }
 }
